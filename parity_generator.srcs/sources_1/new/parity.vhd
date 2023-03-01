c9c7e143845516bc7c2a7bf2841ecb3c2bcd8d3a
mysteriousCzrsX <cezartusken47@gmail.com>
2023-03-01 16:22:03 +0100

    Initial commit

---
+++ parity_generator.srcs/sources_1/new/parity.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01.03.2023 16:17:44
+-- Design Name: 
+-- Module Name: parity - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( sw_i : in STD_LOGIC_VECTOR (7 downto 0);
+        led7_an_o : out STD_LOGIC_VECTOR (3 downto 0);
+        led7_seg_o : out STD_LOGIC_VECTOR (7 downto 0));
+end top;
+
+architecture Behavioral of top is
+
+begin
+
+
+end Behavioral;
