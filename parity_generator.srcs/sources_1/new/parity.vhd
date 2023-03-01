1a5f5573e8f649b6c10b701100df488515e1bc55
mysteriousCzrsX <cezartusken47@gmail.com>
2023-03-01 19:32:13 +0100

    V1 parity calculation

--- parity_generator.srcs/sources_1/new/parity.vhd
+++ parity_generator.srcs/sources_1/new/parity.vhd
@@ -32,14 +32,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity top is
-    Port ( sw_i : in STD_LOGIC_VECTOR (7 downto 0);
+    Port(sw_i : in STD_LOGIC_VECTOR (7 downto 0);
         led7_an_o : out STD_LOGIC_VECTOR (3 downto 0);
         led7_seg_o : out STD_LOGIC_VECTOR (7 downto 0));
 end top;
 
 architecture Behavioral of top is
-
+    signal parity_bit : STD_LOGIC;
 begin
-
-
+    led7_an_o <= "1110";
+    parity_bit <= sw_i(7) xor sw_i(6) xor sw_i(5) xor sw_i(4) xor sw_i(3) xor sw_i(2) xor sw_i(1) xor sw_i(0);
+    led7_seg_o <= "01100001" when (parity_bit = "0") else "00000011"
 end Behavioral;
